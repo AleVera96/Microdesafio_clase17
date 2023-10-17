3b79c02e131b5d7e6a54c45c7ba7b2fc070fc3ae
AleVera96 <alejandrovera.av96@gmail.com>
2023-10-17 10:20:41 -0300

    Cree la estructura de carpetas, se agrego public-img, views-home

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const app = express ()
 const path = require("path")
 
 app.listen(3000,()=> console.log("Levantando server en 3000"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname , ""))
+})
