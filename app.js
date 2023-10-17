a81d73170c007025bf3755077245e2119bdd26ad
AleVera96 <alejandrovera.av96@gmail.com>
2023-10-17 10:36:49 -0300

    Enlacé el home desde app, instale nodemon, levante y probe el servidor

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const path = require("path")
 app.listen(3000,()=> console.log("Levantando server en 3000"));
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname , ""))
+    res.sendFile(path.join(__dirname , "/views/home.html"))
 })
